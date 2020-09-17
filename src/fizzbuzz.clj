4992928d1e01dee6a1016d8cbc346ab36d97bc31
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-17 21:26:52 +0300

    buzzin

--- src/fizzbuzz.clj
+++ src/fizzbuzz.clj
@@ -10,3 +10,31 @@
      :else (str i)))
   ([start end]
    (apply str (interpose "\n" (map fizzbuzz (range start end))))))
+
+
+(def fizz? #(zero? (mod % 3)))
+(def buzz? #(zero? (mod % 5)))
+(def fizzbuzz? #(zero? (mod % (* 3 5))))
+
+(def fuzzbuzz2
+  #(cond
+     (fizzbuzz? %) "fizzbuzz"
+     (fizz? %) "fizz"
+     (buzz? %) "buzz"
+     :else %))
+
+
+(comment
+  (fizz? 3)
+  (fizz? 4)
+  (buzz? 4)
+  (buzz? 5)
+  (fizzbuzz? 10)
+  (fizzbuzz? 15)
+  (map fizzbuzz (range 1 101)))
+
+
+
+
+
+
